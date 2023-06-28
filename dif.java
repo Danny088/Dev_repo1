6e44583ab58cf1d94693fceaa6897cf7ef63419e
Daniel Adagha <Danny08olumese@gmail.com>
2023-06-28 15:30:37 +0100

    Initial Commit

---
+++ dif.java
@@ -0,0 +1,9 @@
+public class Index {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 2;
+        int sum = num1 + num2;
+        System.out.println("The sum of 2 + 2 is: " + sum);
+    }
+}
+ 
