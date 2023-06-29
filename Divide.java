20979a0771ff7c73ac050f2198b33bfbb29765b9
Daniel Adagha <Danny08olumese@gmail.com>
2023-06-29 14:28:26 +0100

    Added new feature of divide

---
+++ Divide.java
@@ -0,0 +1,9 @@
+public class Divide {
+    public static void main(String[] args) {
+        int dividend = 100;
+        int divisor = 5;
+        int quotient = dividend / divisor;
+        System.out.println("The result of the division 100/5 is: " + quotient);
+    }
+}
+ 
